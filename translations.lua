a3c2fec123e47d7a1786c984d04433e2a2870133
Yevhenii Kozii <varmid@gmail.com>
2023-11-27 13:41:39 +0200

    lookupNpcs

--- translations.lua
+++ translations.lua
@@ -3,21 +3,17 @@ local L = {}
 L.locale = "ukUA"
 L.localeName = "Українська"
 
-L.itemLookup = {
-[50432] = "Штука з вовка",
-}
+--L.itemLookup = {
+--[50432] = "Штука з вовка",
+--}
 
-L.questLookup = {
-[7] = {"Зачистка табору кобольдів", {"Твоє перше завдання буде пов'язане із зачищенням, {ім'я}. Клан кобольдів заполонив ліси на півночі. Направляйся туди та знищ усіх кобольдів-шкідників, що зустрінуться на твоєму шляху. Зменш їхню популяцію настільки, щоб одного дня ми змогли остаточно вигнати їх з Північнокраю."}, {"Убийте 10 кобольдів-шкідників, а потім поверніться до маршала Макбрайда."}},
-[33] = {"Вовки за порогом", {"Ich hasse diese grässlichen Waldwölfe! Aber Wolfsteaks esse ich für mein Leben gern. Bringt mir zähes Wolfsfleisch, dann gebe ich Euch etwas dafür, das Euch von Nutzen sein wird.","Zähes Wolfsfleisch bekommt Ihr, wenn Ihr die Waldwölfe und jungen Wölfe jagt, die in Nordhain durch das Land streifen."}, {"Bringt Eagan Peltskinner, den Ihr vor der Abtei von Nordshire findet, 8 Stücke zähes Wolfsfleisch."}},
-[5261] = {"Іган Пелтскіннер", {"Аврора Бореаліс"}, {"Бореаліс Аврора"}},
+--L.questLookup = {
+--[7] = {"Зачистка табору кобольдів", {"Твоє перше завдання буде пов'язане із зачищенням, {ім'я}. Клан кобольдів заполонив ліси на півночі. Направляйся туди та знищ усіх кобольдів-шкідників, що зустрінуться на твоєму шляху. Зменш їхню популяцію настільки, щоб одного дня ми змогли остаточно вигнати їх з Північнокраю."}, {"Убийте 10 кобольдів-шкідників, а потім поверніться до маршала Макбрайда."}},
+--[33] = {"Вовки за порогом", {"Ich hasse diese grässlichen Waldwölfe! Aber Wolfsteaks esse ich für mein Leben gern. Bringt mir zähes Wolfsfleisch, dann gebe ich Euch etwas dafür, das Euch von Nutzen sein wird.","Zähes Wolfsfleisch bekommt Ihr, wenn Ihr die Waldwölfe und jungen Wölfe jagt, die in Nordhain durch das Land streifen."}, {"Bringt Eagan Peltskinner, den Ihr vor der Abtei von Nordshire findet, 8 Stücke zähes Wolfsfleisch."}},
+--[5261] = {"Іган Пелтскіннер", {"Аврора Бореаліс"}, {"Бореаліс Аврора"}},
+--}
 
-}
-
-L.npcNameLookup = {
-[6] = "Кобольд-шкідник",
-[196] = "Іґан Гарбар"
-}
+--L.npcNameLookup = {}
 
 L.objectLookup = {}
 
