f1dc41fa5dd28b5a3d057aea1e515d70fdfa6009
Yevhenii Kozii <varmid@gmail.com>
2023-12-12 19:25:23 +0200

    Add quest objectives for Classic

---
+++ Translations/ExtraObjectives/ClassicObjectives.lua
@@ -0,0 +1,60 @@
+---@type l10n
+local l10n = QuestieLoader:ImportModule("l10n")
+
+local classicObjectiveLocales = {
+    ["Summon Dagun the Ravenous using an Enchanted Sea Kelp"] = "Викличте Даґуна-Ненажеру за допомогою зачарованих морських водоростей", -- 735 736,
+    ["Use a Fresh Carcass at the Flame of Uzel"] = "Покладіть свіжу тушу на полум'я Узеля", -- 1136
+    ["Fish for Darkshore Groupers"] = "Виловіть окунів Темнобережжя", -- 1141
+    ["Slay Gelkis centaur to increase your reputation with the Magram Clan"] = "Вбивайте кентаврів племені Ґелкіс, щоб покращити відносини з племенем Маґрам", -- 1367
+    ["Slay Magram centaur to increase your reputation with the Gelkis Clan"] = "Вбивайте кентаврів племені Маґрам, щоб покращити відносини з племенем Ґелкіс", -- 1368
+    ["Use the War Horn Mouthpiece to summon Khan Hratha"] = "Скористайтесь мундтуком, щоб засурмити в бойовий ріг і викликати хана Грата", -- 1380 1381
+    ["Fish for Gaffer Jacks"] = "Виловіть світлоштепселі", -- 1579
+    ["Fish for Electropellers"] = "Виловіть електропелери", -- 1580
+    ["Use Cantation of Manifestation to reveal Rift Spawn. Use Chest of Containment Coffers on stunned Rift Spawn"] =
+        "Скористайтесь закляттям прояву, щоб побачити породження розлому. За допомогою скрині стримування спіймайте породження, поки воно приголомшене.", -- 1920 & 1960
+    ["Step 1: obtain the Yellow Punch Card. You need the White Punch Card."] = "Крок 1: отримайте жовту перфокарту. Для цього вам потрібна біла перфокарта.", -- 2930
+    ["Step 2: obtain the Blue Punch Card. You need the Yellow Punch Card."] = "Крок 2: отримайте синю перфокарту. Для цього вам потрібна жовта перфокарта.", -- 2930
+    ["Step 3: obtain the Red Punch Card. You need the Blue Punch Card."] = "Крок 3: отримайте червону перфокарту. Для цього вам потрібна синя перфокарта.", -- 2930
+    ["Step 4: obtain the Prismatic Punch Card. You need the Red Punch Card."] = "Крок 4: отримайте призматичну перфокарту. Для цього вам потрібна червона перфокарта.", -- 2930
+    ["Slay Vale Screechers and use Yeh'kinya's Bramble on their corpse."] = "Вбийте долинних верескунів та скористайтесь жезлом Є'кіньї на їхніх трупах.", -- 3520
+    ["Use the Ward of the Defiler to summon Razelikh."] = "Скористайтесь оберегом Губителя, щоб викликати Разеліха.", -- 3628
+    ["Teleport to the top of the mountain."] = "Телепортуйтесь на вершину гори.", -- 3628
+    ["Place the Bait in front of Miblon Snarltooth."] = "Покладіть приманку перед Міблоном Хрипозубом.", -- 3909
+    ["Use the Altered Black Dragonflight Molt on Bael'gar's corpse."] = "Скористайтесь зміненою шкірою чорного дракона на рештках Баєль'Ґара.", -- 4024
+    ["Use the Gorishi Queen Lure."] = "Скористайтесь приманкою для королеви Ґоріші.", -- 4507
+    ["Collect eggs using the Collectronic Module."] = "Зберіть яйця за допомогою яйцезбирального пристрою.", -- 4735
+    ["Summon Xabraxxis once you have the required items from the Blackwood Stores."] = "Зберіть необхідні предмети в таборах Чорнолісних та викличте Ксабраксіса.", -- 4763
+    ["Use the Flames of the Black Flight over it to create the Seal."] = "Скористайтесь полум'ям чорного дракона, щоб завершити печатку.", -- 4743
+    ["Beat Emberstrife till his will is broken, then place the Unforged Seal of Ascension before him and use the Orb of Draconic Energy."] =
+        "Бийте Вуглечвара, поки його воля не буде зломлена, потім покладіть перед ним нековану печатку Вознесіння і скористайтесь сферою драконічної енергії.", -- 4743
+    ["Lure the Kodos to Smeed Scrabblescrew."] = "Приведіть кодо до Смііда Гвинтодряпа.", -- 5561
+    ["Summon Lord Kragaru"] = "Викличте лорда Краґару", -- 6027
+    ["Place the Crate of Ghost Magnets"] = "Розмістіть ящик примарних магнітів", -- 6134
+    ["Use the Lovers' Hearts to summon an Incubus and slay it."] = "Скористайтесь серцями закоханих, щоб викликати інкуба та вбити його.", -- 65597
+    ["Light the Unlit Torch near a fire and use the Burning Torch to set the Archaeologist's Cart on fire."] = "Скористайтесь незапаленим смолоскипом біля вогню і за допомогою палаючого смолоскипа підпаліть возик археолога.", -- 65602
+    ["Use the Withered Scarf to summon an Incubus and slay it."] = "Скористайтесь вицвілою хусткою, щоб викликати інкуба та вбити його.", -- 65603 & 65604
+    ["Slay ghouls to free Darrowshire spirits"] = "Вбийте гулів, щоб звільнити духів Дарроушира", -- 5211
+    ["Exorcise the spirits"] = "Викличте й вигоніть духів", -- 7640
+    ["Plant the Demon Summoning Torch"] = "Розмістіть факел виклику демона", -- 8481
+    ["Use Arcanite Buoy"] = "Скористайтесь арканітовим буєм", -- 8729
+    ["Talk to Silva Fil'naveth to fly back to Darnassus"] = "Поговоріть з Сільвою Філ'навет, щоб полетіти назад до Дарнасу", -- 5931
+    ["Talk to Bunthen Plainswind to fly back to Thunder Bluff"] = "Поговоріть з Бунтеном Рівнинним Вітром, щоб полетіти назад до Громового Бескиду", -- 5932
+    ["Release the kitten near the Jadefire Satyrs' corrupted moonwell."] = "Випустіть кошеня біля оскверненого місячного колодязя нефритожарих сатирів.", -- quest 4506, text from item 12565
+    ["Summon a Templar using a full Twilight set."] = "Викличте храмовника за допомогою повного набору сутінкових атрибутів.", -- 8332 8361
+    ["Summon a Duke using a full Twilight set and neck."] = "Викличте герцога за допомогою повного набору сутінкових атрибутів та медальйону.", -- 8341 8348
+    ["Summon a Lord using a full Twilight set, neck and ring."] = "Викличте лорда Безодні за допомогою повного набору сутінкових атрибутів, медальйону та персня.", -- 8352 9248
+    ["Ask to see the Captain."] = "Поговоріть з капітаном", -- 8507
+    ["Draw the glyphs into the sand to summon the Qiraji Emissary."] = "Намалюйте гліфи на піску, щоб викликати кіразького емісара.", -- 8315
+    ["Place the Relic Bundle in the Town Square."] = "Розмістіть набір реліквій на площі Дарроушира.", -- 5721
+    ["Search the false grave for the Taelan's Hammer."] = "Знайдіть молот Телана біля фальшивої могили.", -- 5781
+    ["Plant the bomb!"] = "Розмістіть вибухівку!", -- 6041
+    ["Use the Empty Worg Pup Cage to capture it."] = "Спіймайте цуценя ворга за допомогою клітки.", -- 4729
+    ["Combine Omokk's Head with the Roughshod Pike."] = "Настроміть голову Омокка на грубий спис", -- 4867
+    ["Use it to instantly kill one nearby ogre."] = "Скористайтесь списом, щоб миттєво вбити огра неподалік.", -- 4867
+    ["Place a tribute at Uther's Tomb"] = "Покладіть підношення до могили Утера", -- 8149
+    ["Place a tribute at Grom's Monument"] = "Покладіть підношення до пам'ятника Ґрому", -- 8150
+}
+
+for k, v in pairs(classicObjectiveLocales) do
+    l10n.translations[k] = v
+end
